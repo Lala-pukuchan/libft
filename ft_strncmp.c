b60e77b69b8b0369a1487ebc689fd9e655ae3478
小林瑠理 <rurukobayashi@kobayashis-MacBook-Pro.local>
2022-05-24 20:26:40 +0900

    added operation for NULL, added the case when n is smaller than str length(strncmp)

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -18,16 +18,18 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	const unsigned char	*p1;
 	const unsigned char	*p2;
 
+	if (!s1 || !s2)
+		return (0);
 	i = 0;
 	p1 = (const unsigned char *)s1;
 	p2 = (const unsigned char *)s2;
-	if (p1[i] == '\0' || p2[i] == '\0')
-		return (p1[i] - p2[i]);
-	while (n-- > 0 && p1[i] != '\0' && p2[i] != '\0')
+	while (p1[i] != '\0' && p2[i] != '\0' && n-- > 0)
 	{
 		if (p1[i] != p2[i])
 			return (p1[i] - p2[i]);
 		i++;
 	}
+	if (n > 0)
+		return (p1[i] - p2[i]);
 	return (0);
 }
