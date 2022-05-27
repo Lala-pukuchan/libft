816c35170c607fc92151b653cd6b6f303b642a9c
小林瑠理 <rurukobayashi@kobayashis-MacBook-Pro.local>
2022-05-27 16:20:47 +0900

    (ft_strtrim)removing char in set from begining and ending

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -15,23 +15,16 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	start;
-	size_t	end;
+	size_t	len;
 	char	*str;
 
-	if (!s1)
+	if (!s1 || !set)
 		return (NULL);
-	if (!set)
-		return ((char *)s1);
-	start = 0;
-	end = ft_strlen(s1);
-	while (s1[start] != '\0' && ft_strchr(set, s1[start]))
-		start++;
-	while (s1[end - 1] != '\0' && ft_strchr(set, s1[end - 1]) && end > start)
-		end--;
-	str = malloc(end - start + 1);
-	if (!str)
-		return (NULL);
-	ft_strlcpy(str, str + start, end - start + 1);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	len = ft_strlen(s1);
+	while (len > 0 && ft_strchr(set, s1[len - 1]))
+		len--;
+	str = ft_substr(s1, 0, len);
 	return (str);
 }
