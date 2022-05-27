81dde7d6a51558f36e881f48690e9383e7fcabf1
小林瑠理 <rurukobayashi@kobayashis-MacBook-Pro.local>
2022-05-27 16:14:32 +0900

    (ft_putnbr_fd)modified to recursive function

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -11,13 +11,22 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*str;
-
-	str = ft_itoa(n);
-	ft_putstr_fd(str, fd);
-	free(str);
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+	}
+	else
+	{
+		if (-10 < n && n < 0)
+			ft_putchar_fd('-', fd);
+		if (n <= -10 || 10 <= n)
+			ft_putnbr_fd(n / 10, fd);
+		n = n % 10;
+		if (n < 0)
+			n *= -1;
+		ft_putchar_fd('0' + n, fd);
+	}
 }
