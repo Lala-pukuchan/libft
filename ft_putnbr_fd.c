c1ef87d818bb8209da287379e5cf51e7dad31180
小林瑠理 <rurukobayashi@kobayashis-MacBook-Pro.local>
2022-06-06 12:26:33 +0900

    modified for dealing with INT_MIN

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -14,19 +14,16 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == INT_MIN)
+	long long	ln;
+
+	ln = (long)n;
+
+	if (ln < 0)
 	{
-		ft_putstr_fd("-2147483648", fd);
+		ft_putchar_fd('-', fd);
+		ln *= (-1);
 	}
-	else
-	{
-		if (-10 < n && n < 0)
-			ft_putchar_fd('-', fd);
-		if (n <= -10 || 10 <= n)
-			ft_putnbr_fd(n / 10, fd);
-		n = n % 10;
-		if (n < 0)
-			n *= -1;
-		ft_putchar_fd('0' + n, fd);
-	}
+	if (9 < ln)
+		ft_putnbr_fd(ln / 10, fd);
+	ft_putchar_fd("0123456789"[ln % 10], fd);
 }
