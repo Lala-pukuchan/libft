a0d403c8417337c61359f7801c47a59d5486b1cc
小林瑠理 <rurukobayashi@kobayashis-MacBook-Pro.local>
2022-06-06 20:41:52 +0900

    casted malloc return value

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -19,7 +19,7 @@ char	*ft_strmapi(char const	*s, char (*f)(unsigned int, char))
 
 	if (!s || !f)
 		return (NULL);
-	str = malloc(ft_strlen(s) + 1);
+	str = (char *)malloc(ft_strlen(s) + 1);
 	if (!str)
 		return (NULL);
 	index = 0;
