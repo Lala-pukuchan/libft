0406fe4e236268542ebf4c83b374e96329d2a281
小林瑠理 <rurukobayashi@kobayashis-MacBook-Pro.local>
2022-06-06 12:31:23 +0900

    used strlcpy & strlcat

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -15,18 +15,13 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*p;
-	size_t	i;
 
 	if (!s1 || !s2)
 		return (NULL);
 	p = (char *)ft_calloc(sizeof(char), ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!p)
 		return (NULL);
-	i = 0;
-	while (*s1 != '\0')
-		p[i++] = *s1++;
-	while (*s2 != '\0')
-		p[i++] = *s2++;
-	p[i] = '\0';
-	return ((char *)p);
+	ft_strlcpy(p, s1, ft_strlen(s1) + ft_strlen(s2) + 1);
+	ft_strlcat(p, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
+	return (p);
 }
