61929af5815ffec5de0c5def795209f8a3c47f61
小林瑠理 <rurukobayashi@kobayashis-MacBook-Pro.local>
2022-06-06 20:58:37 +0900

    changed index from int to size_t

--- ft_itoa.c
+++ ft_itoa.c
@@ -37,7 +37,7 @@ static int	ft_getdigit(unsigned int n)
 	return (count);
 }
 
-static char	*ft_convertint(char *str, int index, unsigned int n, int digit)
+static char	*ft_convertint(char *str, size_t index, unsigned int n, int digit)
 {
 	while (1 < digit)
 	{
@@ -54,7 +54,7 @@ char	*ft_itoa(int n)
 {
 	int				digit;
 	char			*str;
-	int				index;
+	size_t			index;
 	unsigned int	num;
 
 	index = 0;
